8be663e56f4f63bacae8571fadb03d1d3be4a8c2
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2025-09-15 12:18:59 +0100

    Move bag-of-tricks to tools and refactor paths

--- tools/bag-of-tricks/bag-of-tricks.js
+++ tools/bag-of-tricks/bag-of-tricks.js
@@ -11,7 +11,7 @@ let beasts = {
     swimming: []
 };
 
-fetch('../json/beasts.json')
+fetch('../../json/beasts.json')
     .then(response => response.json())
     .then(data => {
         beasts.walking = data.walking;
