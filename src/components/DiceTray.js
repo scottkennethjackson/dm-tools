959e86884412af42f724aae248ae7f81baa741da
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2025-09-25 13:15:42 +0100

    Replace icons

--- src/components/DiceTray.js
+++ src/components/DiceTray.js
@@ -103,11 +103,11 @@ const DiceTray = () => {
           <svg
             aria-hidden="true"
             focusable="false"
-            className="size-8 fill-white group-hover:fill-red group-active:fill-activered"
+            className="size-10 fill-white group-hover:fill-red group-active:fill-activered"
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 384 512"
+            viewBox="0 -960 960 960"
           >
-            <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+            <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
           </svg>
         </button>
       )}
@@ -316,11 +316,11 @@ const DiceTray = () => {
           <svg
             aria-hidden="true"
             focusable="false"
-            className="size-8 fill-red group-active:fill-activered"
+            className="size-10 fill-red group-active:fill-activered"
             xmlns="http://www.w3.org/2000/svg" 
-            viewBox="0 0 512 512"
+            viewBox="0 -960 960 960" 
           >
-            <path d="M125.7 160l50.3 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L48 224c-17.7 0-32-14.3-32-32L16 64c0-17.7 14.3-32 32-32s32 14.3 32 32l0 51.2L97.6 97.6c87.5-87.5 229.3-87.5 316.8 0s87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3s-163.8-62.5-226.3 0L125.7 160z" />
+            <path d="M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q-32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t-196 67Z" />
           </svg>
         </button>
       </div>
